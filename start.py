bba7c5d84cc7e87ef5f422d99678c30c39963a1a
anhmtk <anhnguyen.nk86@gmail.com>
2025-06-01 17:58:44 +0700

    Update start.py

--- start.py
+++ start.py
@@ -2,5 +2,7 @@ import os
 import uvicorn
 from main_webhook import webhook_app
 
-port = int(os.environ.get("PORT", 10000))
+# Render sẽ tự truyền PORT qua biến môi trường, không cần set cứng
+port = int(os.environ.get("PORT", 8000))
+
 uvicorn.run(webhook_app, host="0.0.0.0", port=port)
